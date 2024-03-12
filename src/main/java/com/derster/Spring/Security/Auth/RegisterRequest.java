14226174ec3e685464866d1cdbea8b6dcf65dd48
modestek <mkouadio@weblogy.com>
2024-03-12 17:34:27 +0000

    Spring Security

---
+++ src/main/java/com/derster/Spring/Security/Auth/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.derster.Spring.Security.Auth;
+
+import com.derster.Spring.Security.Entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
---
+++ src/main/java/com/derster/Spring/Security/Token/TokenType.java
@@ -0,0 +1,5 @@
+package com.derster.Spring.Security.Token;
+
+public enum TokenType {
+    BEARER
+}
